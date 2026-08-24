9576df085e184b88b38e0f2159cab2beeb7a1897
Amrutha R <amrutha2132005@gmail.com>
2026-08-24 20:47:06 +0530

    Configure MySQL for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,12 @@
 # config.py
 
+import os
+
+
 MYSQL_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "amrutha",
-    "database": "crime_prediction_db",
-    "port": 3306
+    "host": os.getenv("MYSQL_HOST", "localhost"),
+    "user": os.getenv("MYSQL_USER", "root"),
+    "password": os.getenv("MYSQL_PASSWORD", ""),
+    "database": os.getenv("MYSQL_DATABASE", "crime_prediction_db"),
+    "port": int(os.getenv("MYSQL_PORT", "3306"))
 }
